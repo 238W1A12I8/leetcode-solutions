77d3299d3e11316c6945a9fe69e9da81f05424f9
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-03-03 16:29:03 +0530

    Time: 2 ms (6.26%), Space: 47.6 MB (99.55%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,12 @@
 class Solution {
     public void reverseString(char[] s) {
-        int l=0;
-        int r=s.length-1;
-        while(l<r){
-            char temp=s[l];
-            s[l]=s[r];
-            s[r]=temp;
-            l++;
-            r--;
+        StringBuilder st=new StringBuilder();
+        for(char i:s){
+            st.append(i);
+        }
+        st.reverse();
+        for (int i = 0; i < s.length; i++) {
+            s[i] = st.charAt(i);
         }
     }
 }
