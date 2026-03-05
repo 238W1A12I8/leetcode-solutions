88b296636a3bb446ae49c8c918e1adf12159a5e2
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-03-05 10:43:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,26 +1,20 @@
 class Solution {
     public int longestPalindrome(String s) {
         char ss[]=s.toCharArray();
-        Arrays.sort(ss);
-        int left=0;
-        int right=1;
-        boolean found=false;
-        int length=0;
-        while(right<ss.length){
-            if(ss[left]==ss[right]){
-                length+=2;
-                left=right+1;
-                right=left+1;
+        int count=0;
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<ss.length;i++){
+            if(set.contains(ss[i])){
+                set.remove(ss[i]);
+                count+=2;
             }
             else{
-                found=true;
-                left++;
-                right++;
+                set.add(ss[i]);
             }
         }
-        if(left<ss.length) found=true;
-        if(found) length+=1;
-        return length;
-
+        if(!set.isEmpty()){
+            count+=1;
+        }
+        return count;
     }
 }
