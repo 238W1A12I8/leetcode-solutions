c9b245873d08d46292ec2a1147aa8041292d46f1
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-03-08 08:19:57 +0530

    Time: 1 ms (23.08%), Space: 43 MB (66%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char ss[]=s.toCharArray();
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<ss.length;i++){
+            if(ss[i]>='A' && ss[i]<='Z'){
+                ss[i]=(char)(ss[i]+32);
+            }
+            str.append(ss[i]);
+        }
+        return str.toString();
     }
 }
