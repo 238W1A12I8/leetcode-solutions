b4a90bc0a72a32de79f08c7eef6af0c8d029896e
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-15 15:19:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         int n=nums.length;
-        Map<Integer,Integer> map=new HashMap<>();
+        int ans=0;
+        int freq[]=new int[n+1];
         for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            freq[nums[i]]++;
         }
-        for(int i=0;i<n;i++){
-            if(map.get(nums[i])>1)
-            {
-                return  nums[i];                                                         
+        for(int i=0;i<n+1;i++){
+            if(freq[i]>=2){
+                ans=i;
             }
         }
-        return 0;
+        return ans;
     }
 }
