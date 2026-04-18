c1566c6f26171405435f3e5aa28fa9665c4e2d8f
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-18 21:36:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,28 +1,17 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null) return true;
+        Stack<Integer> st=new Stack<>();
         ListNode temp=head;
-        int count=0;
         while(temp!=null){
-            count=count+1;
+            st.push(temp.val);
             temp=temp.next;
         }
-        int h[]=new int[count];
         temp=head;
-        int i=0;
         while(temp!=null){
-            h[i]=temp.val;
-            temp=temp.next;
-            i++;
-        }
-        int l=0;
-        int r=h.length-1;
-        while(l<r){
-            if(h[l]!=h[r]){
+            if(temp.val!=st.pop()){
                 return false;
             }
-            l++;
-            r--;
+            temp=temp.next;
         }
         return true;
     }
