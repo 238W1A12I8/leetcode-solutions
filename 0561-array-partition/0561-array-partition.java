b04ac0a61dfd5b825dc6b9004b1eba029343e80a
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-19 09:41:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,22 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
+        int count[]=new int[20001];
         int n=nums.length;
+        for(int i=0;i<n;i++){
+             count[nums[i] + 10000]++;
+        }
+        boolean flag=true;
         int sum=0;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i+=2){
-            sum+=nums[i];
+        for(int i=0;i<count.length;i++){
+        if(count[i]>0){
+            while(count[i]>0){
+                if(flag){
+                    sum+=i-10000;
+                }
+                flag=!flag;
+                count[i]--;
+            }
+        }
         }
         return sum;
     }
