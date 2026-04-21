99300d43b2f1b716f4eb7c605d1e7addeb63f38e
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-21 14:42:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,15 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n=nums.length;
-        if(n<=2){
-            return n;
-        }
-        int k=2;
-        for(int i=2;i<n;i++){
-            if(nums[i]!=nums[k-2]){
-                nums[k++]=nums[i];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int count = map.getOrDefault(nums[i], 0);
+            if (count < 2) {
+                nums[index++] = nums[i];
+                map.put(nums[i], count + 1);
             }
         }
-        return k;
+        return index;
     }
 }
