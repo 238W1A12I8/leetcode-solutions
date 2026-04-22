7cb1a45125289345e9587788b99fa0a169e8d8ad
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-22 11:54:29 +0530

    Time: 12 ms (42.65%), Space: 44.6 MB (38.97%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -1,15 +1,19 @@
 class Solution {
+    public int diff(String a,String b){
+        int n=a.length();
+        int sa=0;
+        for(int i=0;i<n;i++){
+            if(a.charAt(i)==b.charAt(i)){
+                sa=sa+1;
+            }
+        }
+        return n-sa;
+    }
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
         List<String> list=new ArrayList<>();
         for(String q:queries){
             for(String d:dictionary){
-                int dif=0;
-                for(int i=0;i<q.length();i++){
-                    if(q.charAt(i)!=d.charAt(i)){
-                        dif++;
-                    }
-                    if(dif>2) break;
-                }
+                int dif=diff(q,d);
                 if(dif<=2){
                     list.add(q);
                     break;
