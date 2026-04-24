2ea29890d4a3beff884defe552f45581e952ce9f
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-24 10:18:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -1,11 +1,13 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int k = Integer.MAX_VALUE;
-        for (int i=0;i<nums.length;i++) {
-            if (nums[i]==target) {
-                k= Math.min(k,Math.abs(i - start));
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                int res=Math.abs(i-start);
+                map.put(i,res);
             }
         }
-        return k;
+        return Collections.min(map.values());
     }
 }
