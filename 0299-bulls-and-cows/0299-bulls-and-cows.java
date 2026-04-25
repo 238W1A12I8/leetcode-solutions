f78b285179e3ce0faf9f03d32b223d807a1a8edd
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-04-25 07:52:14 +0530

    Time: 1 ms (100%), Space: 43.5 MB (73.36%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int n=guess.length();
+        int b=0;
+        int c=0;
+        int fre[]=new int[10];
+        for(int i=0;i<secret.length();i++){
+            char s=secret.charAt(i);
+            char g=guess.charAt(i);
+            if(s==g){
+                b=b+1;
+            }
+            else{
+                int ss=s-'0';
+                int gg=g-'0';
+                if(fre[ss]<0) c++;
+                if(fre[gg]>0) c++;
+                fre[ss]++;
+                fre[gg]--;
+            }
+        }
+        StringBuilder str=new  StringBuilder();
+        str.append(b);
+        str.append('A');
+        str.append(c);
+        str.append('B');
+        return str.toString();
+    }
+}
