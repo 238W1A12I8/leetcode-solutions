245441f85f4becf346728ae5ef155a791b8197c2
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-05-03 11:17:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
+        Map<Character,Integer> map=new HashMap<>();
+        Map<Character,Integer> map1=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch1:goal.toCharArray()){
+            map1.put(ch1,map1.getOrDefault(ch1,0)+1);
+        }
+        if(!map.equals(map1)){
             return false;
         }
-        String k=s+s;
-        if(k.contains(goal)){
-            return true;
+        for(int i=0;i<s.length();i++){
+            String r=s.substring(i)+s.substring(0,i);
+            if(r.equals(goal)){
+                return true;
+            }
         }
-        else{
-            return false;
-        }
+        return false;
     }
 }
