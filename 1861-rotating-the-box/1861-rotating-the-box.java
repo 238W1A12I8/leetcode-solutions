91e386974c99a56fb2028d884f80685806190994
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-05-06 23:46:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,25 +1,22 @@
 class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
-        int n=boxGrid.length;
-        int m=boxGrid[0].length;
-        char[][] res=new char[m][n];
-        for(int i=0;i<n;i++){
-            int e=m-1;
-            for(int j=m-1;j>=0;j--){
+        int m=boxGrid.length;
+        int n=boxGrid[0].length;
+        char[][] res=new char[n][m];
+        for(char[] row:res){
+            Arrays.fill(row,'.');
+        }
+        for(int i=0;i<m;i++){
+            int e=n-1;
+            for(int j=n-1;j>=0;j--){
                 if(boxGrid[i][j]=='*'){
+                    res[j][m-1-i]='*';
                     e=j-1;
                 }
                 else if(boxGrid[i][j]=='#'){
-                    char temp=boxGrid[i][j];
-                    boxGrid[i][j]=boxGrid[i][e];
-                    boxGrid[i][e]=temp;
+                    res[e][m-1-i]='#';
                     e--;
                 }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                res[j][n-1-i]=boxGrid[i][j];
             }
         }
         return res;
