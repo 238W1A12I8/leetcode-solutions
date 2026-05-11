635bee7c9c5d43b0de6b23895dc7fb58aac7ffa5
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-05-11 13:40:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int n=nums.length;
+        int[] freq=new int[n];
+        for(int num : nums){
+            freq[ num ]++;
+        }
+        int idx=0;
+        for(int i = 0; i<3 ; i++){
+            for(int j =0; j<freq[i]; j++){
+                nums[idx++] = i ;
+            }
+        }
     }
 }
