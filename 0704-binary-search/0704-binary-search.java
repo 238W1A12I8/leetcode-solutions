2f2e502de8c2ee3d45014deb6d2af70a36b81d76
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-05-17 09:09:37 +0530

    Time: 0 ms (100%), Space: 48.3 MB (68.14%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,12 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        boolean flag=false;
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                flag=true;
-                ans=i;
-                return ans;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
             }
         }
         return -1;
