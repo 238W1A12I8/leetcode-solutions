d874a7fffe5ffe43369e9f2707dad3a016826710
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-05-17 08:56:26 +0530

    Time: 1 ms (99.73%), Space: 52.2 MB (46.11%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n=arr.length;
+        if(start<0 || start>=arr.length || arr[start]<0) return false;
+        if(arr[start]==0) return true;
+        int j=arr[start];
+        arr[start]=-arr[start];
+        return   canReach(arr,start+j) || canReach(arr,start-j); 
+    }
+}
