d44d2a777cf67722170f9f4d3adeb5185355e11a
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-06-01 23:11:36 +0530

    Time: 1 ms (100%), Space: 44.1 MB (96.56%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,15 +1,22 @@
 class Solution {
     public int minimumCost(int[] cost) {
-        int n = cost.length;
-        Arrays.sort(cost);
-        if (n == 2) {
-            return cost[0] + cost[1];
+        int n=cost.length;
+        int freq[]=new int[101];
+        for(int c:cost){
+            freq[c]++;
         }
-        int total = 0;
-        for (int i = 0; i < cost.length; i += 3) {
-            total += cost[n - 1 - i];
-            if (i + 1 < n) {
-                total += cost[n - 1 - (i + 1)];
+        int total=0;
+        int count=0;
+        for(int i=100;i>=1;i--){
+            while(freq[i]>0){
+                if(count==2){
+                count=0;
+            }
+            else{
+                total+=i;
+                count++;
+            }
+            freq[i]--;
             }
         }
         return total;
