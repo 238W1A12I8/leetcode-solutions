0862db53704bd0b728b79f027ec3a8e77a207f33
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-07-22 14:11:31 +0530

    Time: 3 ms (74.48%), Space: 47.3 MB (36.53%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,13 +1,42 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    HashSet<Integer> set=new HashSet<>();
-    public boolean findTarget(TreeNode root, int k) {
-        return dfs(root,k);
+    List<Integer> list=new ArrayList<>();
+    public void helper(TreeNode root){
+        if(root!=null){
+            helper(root.left);
+            list.add(root.val);
+            helper(root.right);
+        }
     }
-    public boolean dfs(TreeNode node,int k){
-        if(node==null) return false;
-        if(set.contains(k-node.val)) return true;
-        set.add(node.val);
-        return dfs(node.left,k) || dfs(node.right,k);
+    public boolean findTarget(TreeNode root, int k) {
+        helper(root);
+        int left=0;
+        int right=list.size()-1;
+        while(left<right){
+            if(list.get(left)+list.get(right)==k){
+                return true;
+            }
+            else if(list.get(left)+list.get(right)>k){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return false;
     }
 }
