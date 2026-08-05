81d370167c2672ef2501911bf8549dd371eef74c
Murari thogati <153083834+238W1A12I8@users.noreply.github.com>
2026-08-05 09:18:33 +0530

    Time: 15 ms (17.07%), Space: 47.4 MB (10.11%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String shortestCompletingWord(String li, String[] words) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char ch:li.toCharArray()){
+            if(Character.isLetter(ch)){
+                ch=Character.toLowerCase(ch);
+                map.put(ch,map.getOrDefault(ch,0)+1);
+            }
+        }
+        String str="";
+        for(String word:words){
+            Map<Character,Integer> map2=new HashMap<>(map);
+            for(char ch:word.toCharArray()){
+                if(map2.containsKey(ch)){
+                   map2.put(ch,map2.get(ch)-1);
+                   if(map2.get(ch)==0){
+                    map2.remove(ch);
+                   }
+                }
+            }
+            if(map2.isEmpty()){
+                if(str.equals("") || word.length()<str.length()){
+                    str=word;
+                }
+            }
+        }
+        return str;
+    }
+}
